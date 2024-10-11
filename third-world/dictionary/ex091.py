9f70778737ba5e0c088e51ee88d1269d2ed5c10e
Gustavo Weber Balbinot <gustavoweberbalbinot@gmail.com>
2024-10-11 16:37:35 -0300

    ex091
    Created and completed ex091

---
+++ third-world/dictionary/ex091.py
@@ -0,0 +1,17 @@
+#Leia 4 input de 4 jogadores, onde vão jogar um dado
+#Guarde - os resultados no dicionário
+#Mostre - em ordem, o vencedor - quem tirou o maior dado
+
+from random import randint
+from operator import itemgetter
+temp_dictionary = dict()
+player_list = list()
+
+for x in range(0,4):
+    numb_dice = randint(1,6)
+    temp_dictionary[f'player {x + 1}'] = numb_dice
+
+player_list = sorted(temp_dictionary.items(), key=itemgetter(1), reverse=True)
+
+for p,v in player_list:
+    print(f'The player {p} got: {v}')
