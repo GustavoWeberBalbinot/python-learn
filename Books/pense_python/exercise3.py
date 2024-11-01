9e90910e4717db410128e620f2027d28ca52e1bf
Gustavo Weber Balbinot <gustavoweberbalbinot@gmail.com>
2024-11-01 18:01:35 -0300

    Created exercise3
    Working exercise3

---
+++ Books/pense_python/exercise3.py
@@ -0,0 +1,56 @@
+#Escreva uma função chamada right_justify, que receba uma string chamada s como parâmetro e exiba a string com espaços suficientes à frente para que a última letra da string esteja na coluna 70 da tela:
+
+def right_justify(s):
+    justify_spaces = 70 - len(s)
+    print(' '* justify_spaces, f'{s}')
+
+#right_justify(str(input('Enther a world: ')).strip())
+
+#Defina uma função nova chamada do_four que receba um objeto de função e um valor e chame a função quatro vezes, passando o valor como um parâmetro. Deve haver só duas afirmações no corpo desta função, não quatro.
+
+def do_twice(f, v):
+    f(v)
+    f(v)
+
+def print_spam(value):
+    print(value)
+#do_twice(print_spam, 'spam')
+
+def do_four(f,value):
+    do_twice(f,value)
+    do_twice(f,value)
+
+#do_four(print_spam,'spam')
+
+#Escreva uma função que desenhe uma grade com 4 linhas e 4 colunas:
+###
+def rown():
+    print('- '*4, '+ ',  end='')
+
+
+def column():
+    print('|', ' '*9, end='')
+
+
+def do_two(function):
+    function()
+    function()
+    print()
+
+
+def do_three(function):
+    function()
+    do_two(function)
+    print()    
+
+def do_four2(function):
+    do_three(function)
+    do_three(function)
+
+def table(rown, column):
+    print('+ ', end= '')
+    do_two(rown)
+    do_three(column)
+
+table(rown, column)
+
