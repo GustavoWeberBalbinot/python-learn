247b4d304e8d75aba4923ce6ca1507c168d7b0c3
Gustavo Balbinot <gustavoweberbalbinot@gmail.com>
2024-11-25 16:00:26 -0300

    Calculate exercise

--- Books/pense_python/exercise7/exercise7-2.py
+++ Books/pense_python/exercise7/exercise7-2.py
@@ -2,3 +2,14 @@
 
 #Escreva uma função chamada eval_loop que iterativamente peça uma entrada ao usuário, a avalie usando eval e exiba o resultado.
 #Ela deve continuar até que o usuário digite done; então deverá exibir o valor da última expressão avaliada.
+
+
+def eval_loop(expression):
+    while True:
+        print(eval(expression))
+        expression = str(input('Enter with a expression, [done] to stop: ')).strip().lower()
+        if expression == 'done':
+            break
+
+user_expression = str(input('Enter with a expression: '))  
+eval_loop(user_expression)
