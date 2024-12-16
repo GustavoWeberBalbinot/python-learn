6c700929a02964bcb9e64cc15e280ecea4cd6931
Gustavo Balbinot <gustavoweberbalbinot@gmail.com>
2024-12-15 23:02:13 -0300

    Update exercise10-1.py
    Recursion add

--- Books/pense_python/exercise10/exercise10-1.py
+++ Books/pense_python/exercise10/exercise10-1.py
@@ -4,13 +4,25 @@ Escreva uma função chamada nested_sum que receba uma lista de listas de númer
 >>> nested_sum(t)
 21
 '''
-t = [[1, 2], [3], [4, 5, 6]]
+t = [[1, 2,[14,15]], [3], [4, 5, 6]]
 
+#No Recursion, this functions is limited
+'''''
 def nested_sum(t):
     sum_elements = 0
     for sublist in t:
         for elements in sublist:
             sum_elements += elements
+    return sum_elements
+'''
+#With Recursion
+def nested_sum(h):
+    sum_elements = 0
+    if type(h) == int:
+        return h
+    for list_size in h:
+        value = nested_sum(list_size)
+        sum_elements += value
     return sum_elements
 
 print(nested_sum(t))
