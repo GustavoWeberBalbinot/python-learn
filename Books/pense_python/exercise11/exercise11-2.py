4f22646face3b221d596f39272aa968241ea1e69
Gustavo Balbinot <gustavoweberbalbinot@gmail.com>
2025-01-25 14:11:05 -0300

    Update exercise11-2.py

--- Books/pense_python/exercise11/exercise11-2.py
+++ Books/pense_python/exercise11/exercise11-2.py
@@ -1,3 +1,18 @@
 '''
-
+Leia a documentação do método de dicionário setdefault e use-o para escrever uma versão mais concisa de invert_dict.
 '''
+
+dict_teste = {'a': 1, 'b': 1, 'c': 2, 'd': 4, 'e': 3, 'f': 4}
+
+def invert_dict(dict_to_invert = dict):
+    inverted = dict()
+    for x in dict_to_invert.keys():
+        value = dict_to_invert.setdefault(x)
+        if  value not in inverted:
+            inverted[value] = [x]
+        else:
+            inverted[value].append(x)
+    return inverted
+
+
+print(invert_dict(dict_teste))
