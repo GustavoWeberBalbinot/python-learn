66475cfc9d6a6fe32f4db00ea51d3d15b3aa6c55
Gustavo Weber Balbinot <gustavoweberbalbinot@gmail.com>
2025-02-20 17:04:16 -0300

    adjust

--- Books/pense_python/exercise13/exercise13_2.py
+++ Books/pense_python/exercise13/exercise13_2.py
@@ -6,18 +6,21 @@ Exiba o número de palavras diferentes usadas no livro. Compare livros diferente
 '''
 import string
 
-with open('Books/pense_python/exercise13/file13-2.txt', 'r', encoding='utf-8') as file:
-    key = 0
-    txt = ''
-    for line in file:
-        if '*** END OF THE PROJECT GUTENBERG EBOOK' in line: #Skip the end "Header"
-            break
-        if key == 1:
-            txt = txt + line
-            continue
-        if '*** START OF THE PROJECT GUTENBERG EBOOK' in line: #Skip the earlu "Header"
-            key = 1
-    
+my_archive = 'Books/pense_python/exercise13/file13-2.txt'
+
+def read_archive(your_archive):
+    with open(your_archive, 'r', encoding='utf-8') as file:
+        key = 0
+        txt = ''
+        for line in file:
+            if '*** END OF THE PROJECT GUTENBERG EBOOK' in line: #Skip the end "Header"
+                break
+            if key == 1:
+                txt = txt + line
+                continue
+            if '*** START OF THE PROJECT GUTENBERG EBOOK' in line: #Skip the earlu "Header"
+                key = 1
+        return txt
 
 def format_txt(t = str()):
     d = dict()
@@ -28,14 +31,15 @@ def format_txt(t = str()):
     d[8220] = None #Unicode to “
     d[8221] = None #Unicode to ”
     t = t.translate(d)
-    t = t.replace(' ', string.whitespace).replace(' \t', '')
+    t = t.replace(' ', string.whitespace).replace(' \t', '').replace('\t', '').replace('-', '').replace('—', '')
     t = t.replace('\x0b\x0c', '') #\x0b\x0c === ♂♀
     t = t.lower()
     t = t.splitlines()
     for line in t:
         d_words[line] = d_words.get(line,0) + 1
     d_words.pop('')
+    print(f'The amount of diferrent words is:{len(d_words)}')
     return d_words
 
-
+txt = read_archive('Books/pense_python/exercise13/file13-2.txt')
 format_txt(txt)
