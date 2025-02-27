6a4ed9f9ef5d6238d79f99fbcd00dcaa4685bbdb
Gustavo Weber Balbinot <gustavoweberbalbinot@gmail.com>
2025-02-27 16:56:54 -0300

    Update exercise9_5.py

--- Books/pense_python/exercise9/exercise9_5.py
+++ Books/pense_python/exercise9/exercise9_5.py
@@ -6,16 +6,29 @@ word1 = "education"  # Deve retornar True para "aeiou"
 word2 = "hello"      # Deve retornar False para "aeiou"
 word3 = "facetiously"  # Deve retornar True para "aeiouy"
 
-required1 = "aeiou"
-required2 = "aeiouy"
+required1 = "aeiou" #ã é á... not enter in the filter
+required2 = "aeiouy"  
+
+with open('Books/pense_python/exercise9/moby_words.txt', 'r', encoding='utf-8') as file:
+    txt = file.read()
 
 
-def uses_all(word, letters):
+def uses_all(word = str(), letters = str()):
     letters = list(letters)
-    for lt in letters:
-        if lt not in word:
-            return False
+    word = word.split() #This part is so good to separate the archive in list to many strings
+    for words in word: #This part is to get the string in the list
+        for lt in letters:
+            if lt not in words:
+                return False
     return True
 
+def count_uses_all(word = str(), letters = str()):
+        count_True = 0
+        word = word.split()
+        for w in word:
+            if uses_all(w, letters):
+                  count_True += 1
+        return count_True
+             
 
-print(uses_all(word2, required1))
+print(count_uses_all(txt, required2))
