6acd9a5e20531be5fe98ead78338c81283d8d4e1
Gustavo Weber Balbinot <gustavoweberbalbinot@gmail.com>
2025-03-12 08:49:14 -0300

    In working
    in working

---
+++ Books/pense_python/exercise13/exercise13_6.py
@@ -0,0 +1,37 @@
+'''
+O Python fornece uma estrutura de dados chamada SET, que fornece muitas operações de conjunto. Você pode ler sobre elas em “Conjuntos”, na página 274, ou ler a documentação em http://docs.python.org/3/library/stdtypes.html#types-set.
+
+Escreva um programa que use a subtração de conjuntos para encontrar palavras no livro que não estão na lista de palavras.
+'''
+
+import string
+
+
+
+def read_archive(file_name):
+    hist = dict()
+    file = open(file_name, 'r', encoding='utf-8')
+    for line in file:
+        analitic_line(line, hist)
+    file.close()
+    return hist
+
+def analitic_line(line,hist):
+    line = line.replace('-', '')
+    for word in line.split():
+         word = word.strip(string.whitespace + string.punctuation)
+         word = word.lower()
+         hist[word] = hist.get(word,0) + 1
+
+
+def compare_archives(file1,file2):
+    hist = dict()
+    for key in file1:
+         if key not in file2:
+              hist[key] = None
+    return hist.keys()
+
+
+file1 = read_archive('Books/pense_python/exercise13/file13-2.txt')
+file2 = read_archive('Books/pense_python/exercise13/moby_words.txt')
+print(compare_archives(file1,file2))
