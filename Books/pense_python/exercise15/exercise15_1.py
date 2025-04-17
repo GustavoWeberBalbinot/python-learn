8b4e70a961a73e409232a52afea640b3e4f6355a
Gustavo Weber Balbinot <gustavoweberbalbinot@gmail.com>
2025-04-16 21:59:10 -0300

    Update exercise15_1.py

--- Books/pense_python/exercise15/exercise15_1.py
+++ Books/pense_python/exercise15/exercise15_1.py
@@ -10,6 +10,8 @@ Escreva uma função chamada rect_in_circle, que tome um Circle e um Rectangle e
 Escreva uma função denominada rect_circle_overlap, que tome um Circle e um Rectangle e retorne True, se algum dos cantos do retângulo cair dentro do círculo. Ou, em uma versão mais desafiadora, retorne True se alguma parte do retângulo cair dentro do círculo.
 '''
 
+import math
+
 class Circle():
     """Is a reference to a Circle
     attributes: center, radius
@@ -25,8 +27,38 @@ class Point():
     attributes: x, y
     """
 
-def point_in_circle(circle, rectangle):
-    print('')
+def point_in_circle(circle, point):
+    euclidean_distance = math.sqrt(
+        math.pow((point.x - circle.center.x),2) +  math.pow((point.y - circle.center.y),2)
+        )
+    if euclidean_distance <= circle.radius:
+        print(f'The point, {point.x}x and {point.y}y, there in the circle.')
+    else:
+        print(f'The point, {point.x}x and {point.y}y, not there in the circle.')
+    return True
+
+
+def rect_in_circle(circle, rectangle):
+    '''
+    If point in rectangle there in the center
+    '''
+    count = 0
+    rectangle.point = Point()
+    px_right = rectangle.corner.x + (rectangle.width/2)
+    py_right= rectangle.corner.y + (rectangle.height/2)
+    px_left = rectangle.corner.x - (rectangle.width/2)
+    py_left = rectangle.corner.y - (rectangle.height/2)
+    rectangle_points = [px_right,py_right], [px_right,py_left], [px_left,py_right], [px_left,py_left]
+    for x in rectangle_points:
+        p = Point()
+        p.x = x[0]
+        p.y = x[1]
+        if point_in_circle(circle,p):
+            count += 1
+    if count == 4:
+        return True
+    return False
+
 
 cc = Circle()
 cc.center = Point()
@@ -41,4 +73,10 @@ rg.height = 100
 rg.corner.x = 50
 rg.corner.y = 50
 
-point_in_circle(cc,rg)
+p = Point()
+p.x = 74
+p.y = 100
+
+#value = point_in_circle(cc,p)
+#print(value)
+print(rect_in_circle(cc, rg))
