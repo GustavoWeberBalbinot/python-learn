5b8bad2fe8b099b35037cb7f5d0963119a5497a1
Gustavo Weber Balbinot <gustavoweberbalbinot@gmail.com>
2025-05-16 17:43:57 -0300

    in working

--- Books/pense_python/exercise16/exercise16_2.py
+++ Books/pense_python/exercise16/exercise16_2.py
@@ -13,6 +13,7 @@ Para um desafio um pouco maior, escreva a versão mais geral que calcule o dia e
 import datetime
 from zoneinfo import ZoneInfo
 
+tz = ZoneInfo("America/Sao_Paulo")
 actual_day = datetime.date.today()
 
 
@@ -31,8 +32,27 @@ def actual_week_day(day):
     'month': int(input('Put your birth month: ')),
     'day': int(input('Put your birth day: ')),
     'year': int(input('Put your birth year: '))
+}'''
+
+birthday = {
+    'month': 5,
+    'day': 16,
+    'year': 2004
 }
-people_day = datetime.date(birthday['year'], birthday['month'], birthday['day'])'''
-today_actual = datetime.date.today()
-teste = datetime.timedelta(seconds=11235813)
-print(teste.seconds)
+
+people_day = datetime.date(birthday['year'], birthday['month'], birthday['day'])
+
+def your_age(time):
+    total_days = actual_day - time
+    age = datetime.timedelta(days=total_days.days).days // 365
+    if time.month >= actual_day.month and time.day  >= actual_day.day:
+        next_age = datetime.timedelta(days=time.day + 365) ###Problem its here
+    else:
+        next_age = datetime.timedelta(days=time.day)
+    teste = next_age.days
+    print(f'Your live for: {age} years. Your next birth days is in: {next_age.days - age.days} days, {next_age.seconds * 3600} hours, {next_age.seconds * 60} minutes, {next_age.seconds}')
+    return 
+
+
+your_age(people_day)
+
