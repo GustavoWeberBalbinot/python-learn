cb6a9d1e27f90fdfafa539b8ec4d654e31b455f2
Gustavo Balbinot <gustavoweberbalbinot@gmail.com>
2025-05-26 17:37:55 -0300

    Create 17_exercise_2.py

---
+++ Books/pense_python/exercise17/17_exercise_2.py
@@ -0,0 +1,34 @@
+'''
+Este exercício é uma história com moral sobre um dos erros mais comuns e difíceis de encontrar no Python. Escreva uma definição de classe chamada Kangaroo com os seguintes métodos:
+
+Um método __init__ que inicialize um atributo chamado pouch_contents  em uma lista vazia.
+
+Um método chamado put_in_pouch que receba um objeto de qualquer tipo e o acrescente a pouch_contents.
+
+Um método __str__ que retorne uma representação de string do objeto Kangaroo e os conteúdos de pouch (bolsa).
+
+Teste o seu código criando dois objetos Kangaroo, atribuindo-os a variáveis chamadas kanga e roo, e então acrescentando roo ao conteúdo da bolsa de kanga.
+
+'''
+
+
+
+class Kangaroo():
+
+    def __init__(self, pouch_contents=None):
+        if pouch_contents is None:
+            pouch_contents = []
+        self.pouch_contents = pouch_contents
+    
+    def put_in_pouch(self,other):
+        self.pouch_contents.append(other)
+
+    def __str__(self):
+        values = [str(item) for item in self.pouch_contents]
+        return f"The object Kangaroo with our pouch: {values}"
+
+kanga = Kangaroo()
+roo = Kangaroo()
+kanga.put_in_pouch(roo)
+print(f'ROO:\n{roo}')
+print(f'\nKANGA:\n{kanga}')
